344cb8eee022eb7c3eade53928c47fa24d683e47
devio-at <github@devio.at>
2018-05-21 22:40:43 +0200

    commit to github

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace UniSpell
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.btnSpell = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.colCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.colChar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Font = new System.Drawing.Font("Lucida Sans Unicode", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(451, 88);
+			this.textBox1.TabIndex = 0;
+			// 
+			// btnSpell
+			// 
+			this.btnSpell.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnSpell.Location = new System.Drawing.Point(373, 3);
+			this.btnSpell.Name = "btnSpell";
+			this.btnSpell.Size = new System.Drawing.Size(75, 23);
+			this.btnSpell.TabIndex = 1;
+			this.btnSpell.Text = "Spell";
+			this.btnSpell.UseVisualStyleBackColor = true;
+			this.btnSpell.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.Controls.Add(this.btnSpell);
+			this.panel1.Location = new System.Drawing.Point(12, 106);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(451, 30);
+			this.panel1.TabIndex = 2;
+			// 
+			// panel2
+			// 
+			this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel2.Controls.Add(this.linkLabel2);
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Controls.Add(this.linkLabel1);
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Location = new System.Drawing.Point(12, 408);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(451, 24);
+			this.panel2.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(46, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "(c) 2014";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.Location = new System.Drawing.Point(46, 9);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(33, 13);
+			this.linkLabel1.TabIndex = 1;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "devio";
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(85, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(189, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Unicode™ - Unicode is a trademark of ";
+			// 
+			// linkLabel2
+			// 
+			this.linkLabel2.AutoSize = true;
+			this.linkLabel2.Location = new System.Drawing.Point(269, 9);
+			this.linkLabel2.Name = "linkLabel2";
+			this.linkLabel2.Size = new System.Drawing.Size(71, 13);
+			this.linkLabel2.TabIndex = 3;
+			this.linkLabel2.TabStop = true;
+			this.linkLabel2.Text = "Unicode, Inc.";
+			this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCode,
+            this.colChar,
+            this.colName});
+			this.dataGridView1.Location = new System.Drawing.Point(12, 142);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.Size = new System.Drawing.Size(451, 260);
+			this.dataGridView1.TabIndex = 4;
+			// 
+			// colCode
+			// 
+			this.colCode.HeaderText = "Code";
+			this.colCode.Name = "colCode";
+			this.colCode.ReadOnly = true;
+			this.colCode.Width = 57;
+			// 
+			// colChar
+			// 
+			this.colChar.HeaderText = "Character";
+			this.colChar.Name = "colChar";
+			this.colChar.ReadOnly = true;
+			this.colChar.Width = 78;
+			// 
+			// colName
+			// 
+			this.colName.HeaderText = "Name";
+			this.colName.Name = "colName";
+			this.colName.ReadOnly = true;
+			this.colName.Width = 60;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(475, 444);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "devio UniSpell";
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button btnSpell;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.LinkLabel linkLabel2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn colCode;
+		private System.Windows.Forms.DataGridViewTextBoxColumn colChar;
+		private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace UniSpell
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		Dictionary<string, string> UnicodeNames = null;
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (UnicodeNames == null)
+				InitUnicodeNames();
+
+			var gv = dataGridView1;
+			gv.Rows.Clear();
+
+			foreach(var c in textBox1.Text)
+			{
+				var x = ((int)c).ToString("X4");
+				var s = "[not found]";
+				UnicodeNames.TryGetValue(x, out s);
+
+				//sb.AppendLine(string.Format("U+{1:X4} {0} {2}", c, (int)c, s));
+
+				var row = new DataGridViewRow();
+				row.Cells.Add(new DataGridViewTextBoxCell { Value = string.Format("U+{0:X4}", (int)c) });
+				row.Cells.Add(new DataGridViewTextBoxCell { Value = c });
+				row.Cells.Add(new DataGridViewTextBoxCell { Value = s });
+				gv.Rows.Add(row);
+			}
+		}
+
+		private void InitUnicodeNames()
+		{
+			UnicodeNames = new Dictionary<string, string>();
+
+			var assembly = Assembly.GetExecutingAssembly();
+			var resourceName = "UniSpell.UnicodeData.txt";
+
+			using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					string line;
+					while ((line = reader.ReadLine()) != null)
+					{
+						var rgs = line.Split(";".ToArray());
+
+						UnicodeNames.Add(rgs[0], rgs[1]);
+					}
+				}
+		}
+
+		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			Process.Start("http://devio.at/");
+		}
+
+		private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			Process.Start("http://www.unicode.org/copyright.html");
+		}
+	}
+}
